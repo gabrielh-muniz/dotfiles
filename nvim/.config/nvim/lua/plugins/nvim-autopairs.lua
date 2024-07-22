1e1de139436b67d136539de588589fe47597f777
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2024-07-22 19:33:18 -0300

    feat(plugins): add autopairs and raw LSP

---
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,62 @@
+return {
+  "windwp/nvim-autopairs",
+  event = { "InsertEnter" },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  config = function() 
+    require("nvim-autopairs").setup({
+      check_ts = true,
+    })
+
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+    local cmp = require("cmp")
+
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    -- from documentation
+    local Rule = require("nvim-autopairs.rule")
+    local cond = require("nvim-autopairs.conds")
+    local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+
+    require("nvim-autopairs").add_rules {
+      Rule(' ', ' ')
+    -- Pair will only occur if the conditional function returns true
+        :with_pair(function(opts)
+          -- We are checking if we are inserting a space in (), [], or {}
+          local pair = opts.line:sub(opts.col - 1, opts.col)
+          return vim.tbl_contains({
+            brackets[1][1] .. brackets[1][2],
+            brackets[2][1] .. brackets[2][2],
+            brackets[3][1] .. brackets[3][2]
+          }, pair)
+        end)
+        :with_move(cond.none())
+        :with_cr(cond.none())
+        -- We only want to delete the pair of spaces when the cursor is as such: ( | )
+        :with_del(function(opts)
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          local context = opts.line:sub(col - 1, col + 2)
+          return vim.tbl_contains({
+            brackets[1][1] .. '  ' .. brackets[1][2],
+            brackets[2][1] .. '  ' .. brackets[2][2],
+            brackets[3][1] .. '  ' .. brackets[3][2]
+          }, context)
+        end)
+    }
+    for _, bracket in pairs(brackets) do
+      require("nvim-autopairs").add_rules {
+        -- Each of these rules is for a pair with left-side '( ' and right-side ' )' for each bracket type
+        Rule(bracket[1] .. ' ', ' ' .. bracket[2])
+          :with_pair(cond.none())
+          :with_move(function(opts) return opts.char == bracket[2] end)
+          :with_del(cond.none())
+          :use_key(bracket[2])
+          -- Removes the trailing whitespace that can occur without this
+          :replace_map_cr(function(_) return '<C-c>2xi<CR><C-c>O' end)
+      }
+    end
+  end,
+}
+
---
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      { 'folke/neodev.nvim', opts = {} },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lsp_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover , opts)
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("i", "<C-space>", vim.lsp.buf.completion, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+      end
+      local diagnostic_signs = { Error = "", Warn = "", Hint = "", Info = "", }
+      for type, icon in pairs(diagnostic_signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            on_attach = lsp_attach,
+            capabilities = lsp_capabilities,
+          })
+        end -- configure LSP handlers for each server
+      })
+      -- lua
+      lspconfig.lua_ls.setup {
+        on_attach = lsp_attach,
+        capabilities = lsp_capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+          },
+        },
+      }
+      local open_floating_preview = vim.lsp.util.open_floating_preview
+      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or "rounded" -- Set border to rounded
+        return open_floating_preview(contents, syntax, opts, ...)
+      end
+    end,
+  },
+}
