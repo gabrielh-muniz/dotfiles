0748f8389ec8324a85d0942472ff32f2767fe36f
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2024-07-22 23:26:38 -0300

    feat(lspconfig): remove sign column with icons feature

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -43,11 +43,6 @@ return {
         vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
         vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
       end
-      local diagnostic_signs = { Error = "", Warn = "", Hint = "", Info = "", }
-      for type, icon in pairs(diagnostic_signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
       require("mason-lspconfig").setup_handlers({
         function(server_name)
           lspconfig[server_name].setup({
