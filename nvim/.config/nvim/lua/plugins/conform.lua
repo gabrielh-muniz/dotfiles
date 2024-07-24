b06662e16cfc3f192b1847f16624b0f1945a3118
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2024-07-24 14:00:43 -0300

    feat(plugin): add a formatter

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      formatter_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+    -- format a snippet, if on v mode
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      require("conform").format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end,{ desc = "Format file or range (in v mode)" })
+  end,
+}
